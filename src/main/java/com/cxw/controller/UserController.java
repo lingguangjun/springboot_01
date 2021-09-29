f8fe3736115bd893ee4dd21482b229778b96eafd
cxw <cxw555@qq.com>
2021-09-29 16:29:01 +0800

    first commit3

--- src/main/java/com/cxw/controller/UserController.java
+++ src/main/java/com/cxw/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.cxw.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @GetMapping
+    public String acb(){
+        return "";
+    }
 
 }
